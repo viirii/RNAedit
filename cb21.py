69627f8445926b2cdd4032de6ea4650bfce44fea
viirii <christinebaek@gmail.com>
2017-04-18 00:56:39 -0400

    cell paper 10 upstream/downstream sequence processed

---
+++ cb21.py
@@ -0,0 +1,27 @@
+# http://www.sciencedirect.com/science/article/pii/S0092867414010988 
+# this script process above paper's supplemental material, table s3
+# into intermediate format, for further processing
+# requires 21.txt, which is only the sequences column from mmc3-1.xlsx
+
+pseudoU = "Y" # wild-card for 'pseudo U'
+
+
+def readFile(path):
+  with open(path, "rt") as f:
+      return f.read()
+
+def middleU(filepath) :
+  result = []
+  labels = []
+  read = readFile(filepath)
+  for elem in read.split() :
+    result.append([elem[:10] + pseudoU + elem[11:]])
+
+  ulabel = [0 for x in range(10)] + [1] + [0 for x in range(10)]
+  for i in range(len(read.split())) :
+    labels.append(ulabel)
+
+  return result, labels
+
+
+result, labels = middleU("21.txt")
